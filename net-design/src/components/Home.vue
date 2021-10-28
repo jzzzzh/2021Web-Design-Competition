dc7f0eb83cdf99ca7536f9dc66d3731514667c22
jzh <1499765600@qq.com>
2021-10-28 08:48:25 +0800

    upload

--- net-design/src/components/Home.vue
+++ net-design/src/components/Home.vue
@@ -2,36 +2,6 @@
 	<div id="app">
 		<h1>{{title}}</h1>
 		<p>宋体？</p>
-<!-- 		<a href="#/fyrw">风云人物</a>
-		<a href="#/jdgs">建党故事</a>
-		<a href="#/hhfj">辉煌奋进</a>
-		<a href="#/mhwl">美好未来</a> -->
-<!-- 		<div id="dowebok">
-			<div class="section">
-				<h3>第一屏</h3>
-				<p>fullPage.js — 基本演示</p>
-			</div>
-			<div class="section">
-				<div class="slide">
-					<h3>第二屏的第一屏</h3>
-				</div>
-				<div class="slide">
-					<h3>第二屏的第二屏</h3>
-				</div>
-				<div class="slide">
-					<h3>第二屏的第三屏</h3>
-				</div>
-			</div>
-			<div class="section">
-				<h3>第三屏</h3>
-			</div>
-			<div class="section">
-				<h3>第四屏</h3>
-				<p>这是最后一屏</p>
-			</div>
-		</div>
-		<a href="http://www.dowebok.com/" style="display: none;">dowebok</a>
-		<a href="http://www.dowebok.com/77.html" style="display: none;">jQuery全屏滚动插件fullPage.js</a> -->
 	</div>
 
 </template>
@@ -45,11 +15,6 @@
 			}
 		}
 	}
-	// $(function() {
-	// 	$('#dowebok').fullpage({
-	// 		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90']
-	// 	});
-	// });
 </script>
 
 <style>
--- net-design/src/main.js
+++ net-design/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import $ from 'jquery'
+import 'fullpage.js/vendors/scrolloverflow';
+import VueFullPage from 'vue-fullpage.js';
+Vue.use(VueFullPage);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

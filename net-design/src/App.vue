f419aad9e89f0aa45bc36da5489ecc13523625ae
duwenjie <532923577@qq.com>
2021-11-09 16:25:30 +0800

    dwj 还差背景色改不了

--- net-design/src/App.vue
+++ net-design/src/App.vue
@@ -8,17 +8,19 @@
 			</div>
       <parallax id="jdwyHeader"></parallax>
       <jdwy></jdwy>
-			<div id="fyrw">
-				<index></index>
+			<div id="test1" style="height: 2200px;">
+        <fyrw id="fyrw"></fyrw>
 			</div>
-			<div id="test2">
+			<!-- <div id="test2" style="height: 1600px;"> -->
+				<!-- <hhfj></hhfj> -->
+			<div id="test2" style="height: 1600px;">
+				<hhfj id="hhfj"></hhfj>
+				<!-- <carousel></carousel> -->
+			</div>
+			<div id="test3" style="height: 1200px;">
 				<parallax2 id="dmtx"></parallax2>
 				<province></province>
 			</div>
-			<div id="test3" style="background-color: lavender;">
-				<hhfj id="hhfj"></hhfj>
-			</div>
-      <div id="footer"></div>
 		</div>
 		</div>
 		<goto id="gotoBlock"></goto>
@@ -26,91 +28,88 @@
 </template>
 
 <script>
-	import Home from './components/Home.vue'
-	import jdwy from './components/jdwy.vue'
-	import fyrw from './components/fyrw.vue'
-	import dmtx from './components/dmtx.vue'
-	import parallax from './components/parallax.vue'
-	import goto from './components/goto.vue'
-	import hover from './components/hover.vue'
-	import index from './components/index.vue'
-	import parallax2 from './components/parallax2.vue'
-	import hhfj from './components/hhfj.vue'
-	import carousel from './components/carousel.vue'
-	import hover2 from './components/hover2.vue'
-	import province from './components/province.vue'
-	export default {
-		name: 'App',
-		components: {
-			Home,
-			jdwy,
-			fyrw,
-			dmtx,
-			parallax,
-			goto,
-			hover,
-			index,
-			parallax2,
-			carousel,
-			hover2,
-			hhfj,
-			province,
-		},
-		data() {
-			return {
-				options: {
-					licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-					afterLoad: this.afterLoad,
-					scrollOverflow: true,
-					scrollBar: false,
-					menu: '#menu',
-					sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9',
-						'#b4b8ab'
-					]
-				}
-			}
-		}
-	}
+import Home from './components/Home.vue'
+import jdwy from './components/jdwy.vue'
+import fyrw from './components/fyrw.vue'
+import dmtx from './components/dmtx.vue'
+import parallax from './components/parallax.vue'
+import goto from './components/goto.vue'
+import hover from './components/hover.vue'
+import parallax2 from './components/parallax2.vue'
+import hhfj from './components/hhfj.vue'
+import carousel from './components/carousel.vue'
+import hover2 from './components/hover2.vue'
+import province from './components/province.vue'
+export default {
+  name: 'App',
+  components: {
+    Home,
+    jdwy,
+    fyrw,
+    dmtx,
+    parallax,
+    goto,
+    hover,
+    parallax2,
+    carousel,
+    hover2,
+    hhfj,
+    province
+  },
+  data () {
+    return {
+      options: {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        afterLoad: this.afterLoad,
+        scrollOverflow: true,
+        scrollBar: false,
+        menu: '#menu',
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9',
+          '#b4b8ab', '#ff5500'
+        ]
+      }
+    }
+  }
+}
 </script>
 
 <style>
-	@import url("./assets/font.css");
+@import url("./assets/font.css");
 
-	#app {
-		background-color: #717166;
-		font-family: "Arial", "Microsoft YaHei", "黑体", "宋体", sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
+#app {
+  background-color: #717166;
+  font-family: "Arial", "Microsoft YaHei", "黑体", "宋体", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  position: relative;
+}
 
-		margin-top: 0px;
-		position: relative;
+#gotoBlock {
+  position: fixed;
+  right: 4%;
+  top: 85%;
+}
+
+#headline{
+	font-family: "FZBaiZRZTJW";
+}
+
+#test1 {
+  background-color: #ff5500;
+  height: 1000px;
+}
+
+#test2 {
+		background-color: lavender;
+		height: 1000px;
 	}
 
-	#gotoBlock {
-		position: fixed;
-		right: 4%;
-		top: 85%;
-	}
+#test3 {
+  background-color: lightcoral;
+  height: 1000px;
+}
 
-	#headline {
-		font-family: "FZBaiZRZTJW";
-	}
-
-	#test1 {
-		background-color: #006064;
-	}
-
-	#test2 {
-    background-image:-webkit-linear-gradient(top, #ffc8b7,lightcoral);
-	}
-
-  #test3 {
-    background-image:-webkit-linear-gradient(top, #eeebf4,#E6E6FA);
-  }
-
-  #footer {
-    height: 5%;
-    background-image:-webkit-linear-gradient(top, #E6E6FA,#b1adcc);
-  }
 </style>
